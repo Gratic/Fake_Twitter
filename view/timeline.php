6a3980c71eb681897136ddaa3b25f321e507fe66
Cyril <cyril.gomes@outlook.com>
2022-03-24 17:54:09 +0100

    envore plus

--- view/timeline.php
+++ view/timeline.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8">
     <title>Timeline</title>
-    <link rel="stylesheet" type="text/css" href="<?php urlr("../lib/bootstrap-5.1.3-dist/css/bootstrap.min.css")?>">
-    <link rel="stylesheet" type="text/css" href="<?php urlr("../lib/bootstrap-icons-1.8.1/bootstrap-icons.css")?>">
-    <link rel="stylesheet" type="text/css" href="<?php urlr("../style/style.css")?>">
+    <link rel="stylesheet" type="text/css" href="<?php urlr("lib/bootstrap-5.1.3-dist/css/bootstrap.min.css")?>">
+    <link rel="stylesheet" type="text/css" href="<?php urlr("lib/bootstrap-icons-1.8.1/bootstrap-icons.css")?>">
+    <link rel="stylesheet" type="text/css" href="<?php urlr("style/style.css")?>">
 </head>
 
 <body class="min-vh-100 bg-dark">
