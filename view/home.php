04909e14a749e6edeea16ede54aee2a27ffeba9c
Cyril <cyril.gomes@outlook.com>
2022-03-24 17:26:41 +0100

    test again

--- view/home.php
+++ view/home.php
@@ -5,12 +5,12 @@
 	<title>Home</title>
 	<link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href=<?php dirname(__FILE__)."../lib/bootstrap-5.1.3-dist/css/bootstrap.min.css"?>>
-	<link rel="stylesheet" type="text/css" href=<?php dirname(__FILE__)."../style/style.css"?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo __DIR__."../lib/bootstrap-5.1.3-dist/css/bootstrap.min.css"?>>
+	<link rel="stylesheet" type="text/css" href=<?php echo __DIR__."../style/style.css"?>>
 </head>
 <body>
 
-	<h1><?php dirname(__FILE__)?></h1>
+	<h1><?php echo __DIR__?></h1>
 
 	<button> You think you are clicking me! <>
 	<iframe class="legit" src="http://sere.local/index.php?route=legit"></iframe>
